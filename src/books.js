459f1857aa3d21e7e625468d6de9391ba837e8fc
Nathan Tate <nathantate88@gmail.com>
2024-12-30 11:53:34 -0800

    Kristy's website.

---
+++ src/books.js
@@ -0,0 +1,123 @@
+
+window.BOOKS = [
+{
+href: 'https://www.amazon.com/gp/product/B07VGMZZP2',
+image: 'https://m.media-amazon.com/images/I/512jMpRmHkL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07VGMZZ8J',
+image: 'https://m.media-amazon.com/images/I/51fa9raTnnL.jpg',
+},
+{
+href: 'https://www.amazon.com/Ghost-Second-Chance-Rose-Arbor-ebook/dp/B007IK0KZK',
+image: 'https://m.media-amazon.com/images/I/51BGDDSs7EL.jpg',
+},
+{
+href: 'https://www.amazon.com/Highwayman-Incident-time-travel-romance-Witching-ebook/dp/B00QL15NQ8',
+image: 'https://m.media-amazon.com/images/I/41Qm90FQBwL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B00UM6B7TK',
+image: 'https://m.media-amazon.com/images/I/416EiUKEnbL.jpg',
+},
+{
+href: 'https://www.amazon.com/Pirate-Episode-Witching-Well-Book-ebook/dp/B017DZPT6O',
+image: 'https://m.media-amazon.com/images/I/417Yd1ymvoL.jpg',
+},
+{
+href: 'https://www.amazon.com/dp/B07HQYV596',
+image: 'https://m.media-amazon.com/images/I/41u719NENhL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07KPKVM53',
+image: 'https://m.media-amazon.com/images/I/41ZfYq+yjbL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07NTTDPY9',
+image: 'https://m.media-amazon.com/images/I/41zdy3mv-YL.jpg',
+},
+{
+href: 'https://www.amazon.com/dp/B07RWQKTH7',
+image: 'https://m.media-amazon.com/images/I/51cdXJfR3XL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07CZ67FQY',
+image: 'https://m.media-amazon.com/images/I/51C5HoF9-pL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07FN6V4YN',
+image: 'https://m.media-amazon.com/images/I/51uhxaDh95L.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B07P9FHQ1N',
+image: 'https://m.media-amazon.com/images/I/51aM9F0ipLL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B081Y5RD42',
+image: 'https://m.media-amazon.com/images/I/511ElEz6B8L.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B01MPZJA14',
+image: 'https://m.media-amazon.com/images/I/51sig6WB8NL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B077JGFM46',
+image: 'https://m.media-amazon.com/images/I/51ugdFQrtrL.jpg',
+},
+{
+href: 'https://www.amazon.com/Half-Baked-Better-Romance-Eloise-Alden-ebook/dp/B0882LPHQL',
+image: 'https://m.media-amazon.com/images/I/51WhOX22kJL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B085RGDCFK',
+image: 'https://m.media-amazon.com/images/I/51NP8hOE3vL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B00KQ9JT5C',
+image: 'https://m.media-amazon.com/images/I/41oK9JFf5KL.jpg',
+},
+{
+href: 'https://www.amazon.com/Seadrift-Kristy-Tate-ebook/dp/B0764MVQRR',
+image: 'https://m.media-amazon.com/images/I/51KDYK4BBlL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B00H2CA18U',
+image: 'https://m.media-amazon.com/images/I/51IIH6QzLwL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B00HYLT34W',
+image: 'https://m.media-amazon.com/images/I/51BbmT4E6mL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B00N34SRU0',
+image: 'https://m.media-amazon.com/images/I/51ItvYCioAL.jpg',
+},
+{
+href: 'https://www.amazon.com/Witch-Ways-Kristy-Tate-ebook/dp/B07HHMLH3C',
+image: 'https://m.media-amazon.com/images/I/51XzTqG0qnL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B014QDXSVE',
+image: 'https://m.media-amazon.com/images/I/51svLyGrGqL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B01ATTOP52',
+image: 'https://m.media-amazon.com/images/I/51SfbJSe-TL._SY346_.jpg',
+},
+{
+href: 'https://www.amazon.com/Menagerie-Kristy-Tate-ebook/dp/B01LDTR2PW',
+image: 'https://m.media-amazon.com/images/I/51AhGO+sJnL.jpg',
+},
+{
+href: 'https://www.amazon.com/Melange-Menagerie-Book-Kristy-Tate-ebook/dp/B01N0GS0WB',
+image: 'https://m.media-amazon.com/images/I/51XY3BSRRkL.jpg',
+},
+{
+href: 'https://www.amazon.com/gp/product/B074MJKS1Y',
+image: 'https://m.media-amazon.com/images/I/51SsS+g2lFL.jpg',
+},
+{
+href: 'https://www.amazon.com/dp/B071Y61238',
+image: 'https://images-na.ssl-images-amazon.com/images/I/B1u4o1idMjS._SY300_.png',
+}
+];
